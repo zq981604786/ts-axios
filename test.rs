d6d3ef74d89a7118da96c2ddd628c678ab091707
zhaoquan <43815068+zq981604786@users.noreply.github.com>
2023-07-01 00:14:29 +0800

    Update test.rs

--- test.rs
+++ test.rs
@@ -3,7 +3,7 @@ use sqlparser::dialect::Dialect;
 #[derive(Debug, Default)]
 pub struct TyrDialect;
 
-impl Dialect for TyrDialect{
+impl Dialect for TyrDialect {
     fn is_identifier_start(&self, ch: char) -> bool {
         ('a'..='z').contains(&ch) || ('A'..'Z').contains(&ch) || ch == '-'
     }
